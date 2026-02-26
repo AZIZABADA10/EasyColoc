71f1cc21f9769db9e24fe93fb5f53d98001ef7c0
AZIZABADA10 <abadaaziz288@gmail.com>
2026-02-26 14:46:33 +0000

    feat: creation des fichier des migration pour la base de donnée

---
+++ database/migrations/2026_02_25_144852_add_roles_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('is_admin')->default(false);
+            $table->boolean('is_banned')->default(false);
+            $table->integer('reputation')->default(0);
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn([
+                'is_admin',
+                'is_banned',
+                'reputation'
+            ]);
+        });
+    }
+};
