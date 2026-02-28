674c377090b243ca9b022fa3f62f92e76ccb3174
AZIZABADA10 <abadaaziz288@gmail.com>
2026-02-28 14:35:45 +0000

    Update profile display view

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,3 +1,4 @@
+{{ auth()->user()->hasVerifiedEmail() ? 'EMAIL OK' : 'EMAIL NON VERIFIE' }}
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
